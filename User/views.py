be3d8ba40021d5618bc258404228358eb2f9466b
ErickIgles <erick.almeida2125@gmail.com>
2025-01-10 21:00:27 -0300

    Criação das views para registro de usuário, login page e página inicial.

---
+++ User/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.views.generic import FormView, TemplateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.http import HttpResponse
+
+from django.urls import reverse_lazy
+from .models import CustomUser
+from .forms import CustomUserCreateForm, CustomLoginPage
+# Create your views here.
+
+
+class Home(TemplateView):
+    template_name = 'home.html'
+
+
+class TemplateRegisterUser(FormView):
+    model = CustomUser
+    template_name ='register.html'
+    form_class = CustomUserCreateForm
+    success_url = 'home'
+
+    def form_valid(self, form):
+
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
+
+
+
+class LoginPage(LoginView):
+    template_name = 'registration/login.html'
+    authentication_form = CustomLoginPage
+
+
+    def get_success_url(self):
+        
+        return reverse_lazy('home')
+
+
+    
