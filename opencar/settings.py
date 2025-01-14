dcbc49857d38753372cebaaeb29627e64610e600
ErickIgles <erick.almeida2125@gmail.com>
2025-01-14 18:50:12 -0300

    Adicionando a aplicação 'Cars'

--- opencar/settings.py
+++ opencar/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'stdimage',
     
     'User',
+    'Cars'
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
